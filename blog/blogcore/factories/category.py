3c977ba1cc4e253b86bef8b04b92ee2aa04957b2
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2022-03-17 19:49:29 -0500

    basic crud tested success

--- blog/blogcore/factories/category.py
+++ blog/blogcore/factories/category.py
@@ -1,4 +1,7 @@
 
+# Utils
+from typing import List
+
 # Factory
 import factory
 
@@ -8,9 +11,20 @@ from ..models import Category
 
 class CategoryFactory(factory.django.DjangoModelFactory):
 
+    name = factory.Faker('word')
+    description = factory.Faker('word')
+    slug = factory.Sequence(lambda n: n)
+
     class Meta:
         model = Category
 
-    name = factory.Faker('word')
-    description = factory.Faker('word')
-    slug = factory.Faker('word')
+    @classmethod
+    def create(cls, **kwargs) -> Category:
+        return super().create(**kwargs)
+
+    @classmethod
+    def create_batch(cls, size: int, **kwargs) -> List[Category]:
+        return super().create_batch(size, **kwargs)
+
+
+
--- blog/blogcore/tests/test_category.py
+++ blog/blogcore/tests/test_category.py
@@ -39,3 +39,43 @@ class CategoryTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
         assert Category.objects.count() == 2
         assert Category.objects.exclude(pk=parent.pk).first().parent.pk == parent.pk
+
+    def tests_users_can_retrieve_categories(self):
+
+        CategoryFactory.create_batch(20)
+
+        response = self.client.get('/api/blog/categories/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+
+        assert len(response.data) == 20
+
+    def test_users_can_retrieve_single_categories(self):
+
+        category = CategoryFactory.create(
+            name='Category test',
+            description='Description test'
+        )
+
+        response = self.client.get(f'/api/blog/categories/{category.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+
+        assert response.data
+        assert response.data['name'] == 'Category test'
+        assert response.data['description'] == 'Description test'
+
+    def test_users_can_update_categories(self):
+
+        category = CategoryFactory.create()
+        parent = CategoryFactory.create()
+
+        response = self.client.put(f'/api/blog/categories/{category.id}/', {
+            'name': 'Category test',
+            'description': 'Description test',
+            'parent': parent.id
+        })
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+
+        assert response.data['name'] == 'Category test'
+        assert response.data['description'] == 'Description test'
+        assert response.data['parent'] == parent.id
